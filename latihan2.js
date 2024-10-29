a57d3477e26da7bf80b853f6d540642466d066ca
Angelimnl <142966170+Angelimnl@users.noreply.github.com>
2024-10-29 15:39:57 +0700

    Add files via upload

---
+++ latihan2.js
@@ -0,0 +1,11 @@
+// pembuatan variale
+    var age = 20;
+    let name = "Buddy"; //ini variable type String
+    const country = "Indonesia"; //ini variable type String
+    let isStudent = true; //boolean
+    let person = {name: 'Buddy', age: 20}; //object
+    let numbers = [1, 2, 3, 4, 5]; //array
+    // menampilkan type variable
+    console.log(typeof age, typeof name, typeof country, typeof isStudent, typeof person, typeof numbers);
+    //menampilkan isi variable
+    console.log("nama:"+name,"\n","umur:"+age,"\n","negara:"+country);
---
+++ latihan3.js
@@ -0,0 +1,20 @@
+//Aritmethic Operators
+let sum = 10 + 5; //penjumlahan
+let sub = 10 - 5; //pengurangan
+let mul = 10 * 5; //perkalian
+let div = 10 / 5; //pembagian
+let mod = 11 % 5; //modulus
+let exp = 10 ** 5; //eksponen / pangkat
+console.log(sum, sub, mul, div, mod, exp);
+
+//Comparison Operators
+let isEqual = 5 === "5"; // sama dengan
+let isEqual2 = 5 == "5"; // sama dengan
+let isNotEqual = 10 != "10"; // tidak sama dengan 1 not strict
+let isNotEqual2 = 10 !== "10"; // tidak sama dengan 2 not strict
+let isGreater = 15 > 10; // lebih besar
+let isLess = 5 < 10; // lebih kecil
+let isGreaterEqual = 10 >= 10; // lebih besar sama dengan
+let isLessEqual = 5 <= 10; // lebih kecil sama dengan
+console.log(isEqual, isEqual2, isNotEqual, isNotEqual2, isGreater, isLess, isGreaterEqual, isLessEqual); 
+
